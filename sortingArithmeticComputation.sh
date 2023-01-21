79c356bf3d78816d2ecf11420192a1376ba0f8c0
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-01-21 10:31:20 +0530

    Add: Read the values from the Dictionary into the array

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -18,13 +18,15 @@ compute4=$(($a%$b+$c))
 echo "$a%$b+$c:$compute4"
 
 declare -A dict
-
+declare -a array
 dict[compute1]=$compute1
 dict[compute2]=$compute2
 dict[compute3]=$compute3
 dict[compute4]=$compute4
-
+index=0
 for i in ${!dict[@]}
 do 
-    echo "$i = ${dict[$i]}"
+    array[index++]=${dict[$i]}
 done
+echo "The i results array is"
+echo ${array[@]}
