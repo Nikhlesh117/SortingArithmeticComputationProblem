4ee7c3279b27cc47c40e8d191ecc6bc5e31593d1
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-01-21 09:32:16 +0530

    Add: Write program to take three inputs - a, b & c

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -1,1 +1,6 @@
-echo "Welcome to Sorting Arithmetic Computation program"
+echo "Welcome to Sorting Arithmetic Computation program!!!"
+
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+read -p "Enter third number: " c
+echo "The given three numbers:$a $b $c"
