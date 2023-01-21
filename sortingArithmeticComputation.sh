95d13ff543374b343a5c11d03fb16926e7b30a7d
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-01-21 09:39:18 +0530

    Add: Compute a + b * c

--- sortingArithmeticComputation.sh
+++ sortingArithmeticComputation.sh
@@ -4,3 +4,6 @@ read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
 echo "The given three numbers:$a $b $c"
+
+compute1=$(($a+$b*$c))
+echo "$a+$b*$c:$compute1"
